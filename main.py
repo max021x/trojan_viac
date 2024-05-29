5e46610d5dfc9b537d32384d75c16a4224046000
Max Mohammadi <Max.021persia@gmail.com>
2024-05-29 13:55:05 +0330

    test

---
+++ main.py
@@ -0,0 +1,17 @@
+import ttkbootstrap as tb
+
+
+class Commander :
+  def __init__(self , window) -> None:
+    pass
+
+
+
+
+
+
+
+window = tb.Window(themename='darkly')
+window.geometry('1000x800')
+command = Commander(window = window)
+window.mainloop()
